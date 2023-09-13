309eb9bf554dd151614ea77ad9633b0094b5905d
Caden McCarty <mccartycl@hendrix.edu>
2023-09-12 22:17:38 -0500

    Added BUttons

--- csci340lab4/script.js
+++ csci340lab4/script.js
@@ -12,6 +12,19 @@ $(document).ready(function() {
         }
       });
     });
+    $('.dog').click(function() {
+      $.ajax({
+        dataType: "jsonp",
+        jsonpCallback: "parseQuote",
+        url: "https://api.thecatapi.com/v1/images/search",
+        success: function(results) {
+          $('.fortune').text(results["quoteText"]);
+        },
+        error: function(xhr,status,error) {
+          console.log(error);
+        }
+      });
+    });
   });
 
   
