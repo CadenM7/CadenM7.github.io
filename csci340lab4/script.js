6043d899ddd46378b6ad23f91521b8481c159ea8
Caden McCarty <mccartycl@hendrix.edu>
2023-09-17 13:58:21 -0500

    idk

--- csci340lab4/script.js
+++ csci340lab4/script.js
@@ -1,29 +1,63 @@
 $(document).ready(function() {
-    $('.cat').click(function() {
+  console.log("ready");
+    $('#cclicker').click(function() {
+      console.log("cat clicked");
       $.ajax({
-        dataType: "jsonp",
-        jsonpCallback: "parseQuote",
         url: "https://api.thecatapi.com/v1/images/search",
         success: function(results) {
-          $('.fortune').text(results["quoteText"]);
+          console.log(results[0]);
+          if (results[0]["url"].endsWith(".mp4")) {
+            $('#cat').attr("src", "images/blank.png");
+          } else {
+            $('#cat').attr("src", results[0]["url"]);
+          }
         },
         error: function(xhr,status,error) {
           console.log(error);
         }
       });
     });
-    $('.dog').click(function() {
+    $('#dclicker').click(function() {
       $.ajax({
-        dataType: "jsonp",
-        jsonpCallback: "parseQuote",
-        url: "https://api.thecatapi.com/v1/images/search",
+        dataType: "json",
+        url: "https://random.dog/woof.json",
         success: function(results) {
-          $('.fortune').text(results["quoteText"]);
+          console.log(results["url"]);
+          if (results["url"].endsWith(".mp4")) {
+            $('#dog').attr("src", "images/blank.png");
+          } else {
+            $('#dog').attr("src", results["url"]);
+          }
         },
         error: function(xhr,status,error) {
           console.log(error);
         }
       });
+      $.ajax({
+        dataType: "json",
+        url: 'https://dogapi.dog/api/v2/facts?limit=1',
+        success: function(results) {
+          $('.dogfacts').text(results["data"]["body"]);
+          console.log("Dog Fact");
+          console.log(result);
+        },
+        error: function(xhr,status,error) {
+          console.log(error);
+        }
+      });
+    });
+    console.log("Finding Fact");
+    $.ajax({
+      dataType: "json",
+      url: "https://meowfacts.herokuapp.com/",
+      success: function(results) {
+        console.log("Fact Found");
+        console.log(results)
+        $('.catfacts').text(results["data"]["0"]);
+      },
+      error: function(xhr,status,error) {
+        console.log(error);
+      }
     });
   });
 
